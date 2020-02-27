a0410189e17832615eb9336c531f78f7f83ba731
Nate Dunn <hello@natedunn.net>
2020-02-27 11:04:57 -0600

    Add icons to sticky headers

--- docs/.vuepress/components/PlaygroundDisplay.vue
+++ docs/.vuepress/components/PlaygroundDisplay.vue
@@ -4,9 +4,30 @@
       class="flex flex-col h-full rounded border border-gray-300 overflow-hidden"
     >
       <div
-        class="flex items-center justify-between w-full top-0 sticky py-4 px-8 font-bold text-gray-100 bg-gray-800 border-r border-gray-700"
+        class="flex items-center justify-between w-full top-0 z-10 sticky py-4 px-8 font-bold text-gray-100 bg-gray-800 border-r border-gray-700"
       >
-        Live Preview
+        <div class="flex items-center">
+          <svg
+            class="h-6 w-6 mr-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+            />
+          </svg>
+          Live Preview
+        </div>
         <a role="button" @click="toggleCode">Toggle Code</a>
       </div>
       <div class="flex h-full flex-col justify-between">
--- docs/.vuepress/components/PlaygroundOptions.vue
+++ docs/.vuepress/components/PlaygroundOptions.vue
@@ -5,9 +5,25 @@
   >
     <div class="relative">
       <div
-        class="w-full top-0 sticky py-4 px-8 font-bold text-gray-100 bg-gray-800"
+        class="w-full top-0 sticky z-10 py-4 px-8 font-bold text-gray-100 bg-gray-800"
       >
-        Options
+        <div class="flex items-center">
+          <svg
+            class="h-6 w-6 mr-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+            />
+          </svg>
+
+          Options
+        </div>
       </div>
       <div class="py-10 px-8">
         <slot></slot>
