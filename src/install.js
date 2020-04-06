33195728dcc0716665ae24d2fbf2ac20dda05e41
Nate Dunn <hello@natedunn.net>
2020-04-05 21:30:14 -0500

    Make sure components are getting auto registered

--- src/install.js
+++ src/install.js
@@ -1,14 +1,18 @@
 import Vue from 'vue';
 import Theme from '../z.theme.js';
-import * as components from './elements'
+import * as elements from './elements'
+import * as components from './components'
 import * as Utils from './utils'
 
 const install = (Vue, options = {}) => {
+  for (let key in elements) {
+    let _key = options.prefix ? options.prefix + key : key
+    Vue.component(_key, elements[key])
+  }
   for (let key in components) {
     let _key = options.prefix ? options.prefix + key : key
     Vue.component(_key, components[key])
   }
-
 }
 
 // auto install
