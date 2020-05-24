17d494f92b85d79544be5fbff40a685e78b359f4
Nate Dunn <hello@natedunn.net>
2020-05-24 13:13:48 -0500

    Add basic spinner component

---
+++ src/components/ZSpinner.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="spinner"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    className: {
+      type: String,
+      default: null
+    },
+    isThemeDisabled: {
+      type: Boolean,
+      default: false
+    },
+    variant: {
+      type: [String, Object],
+      default: "_default"
+    },
+    removeClass: {
+      type: [Array, String],
+      default: () => []
+    }
+  }
+};
+</script>
+
+<style lang="postcss">
+.spinner {
+  pointer-events: none;
+  width: 1em;
+  height: 1em;
+  border: 0.15em solid transparent;
+  border-color: transparent;
+  border-top-color: currentColor;
+  border-radius: 50%;
+  animation: spinner 1s linear infinite;
+}
+@keyframes spinner {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
