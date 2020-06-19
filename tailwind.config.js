b58a4c4d4c304b5301aebf99a4a62c1b0b2462d5
Nate Dunn <hello@natedunn.net>
2020-06-19 12:38:30 -0500

    Remove purge option from Tailwind for now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  purge: false,
   variants: {
     backgroundColor: ['dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd', 'responsive', 'hover', 'focus', 'active'],
     textColor: ['dark', 'dark-hover', 'dark-active', 'dark-placeholder', 'active'],
