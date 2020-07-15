ce20dd839bc38758864d43c4a75e12abb8337b5c
Nate Dunn <hello@natedunn.net>
2020-07-14 22:56:23 -0500

    Remove useless class

--- docs/.vuepress/components/PgHeader.vue
+++ docs/.vuepress/components/PgHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <span
-    class="block font-bold flex items-center justify-between mb-4 py-2 text-gray-800 text-xl border-b border-gray-300"
+    class="font-bold flex items-center justify-between mb-4 py-2 text-gray-800 text-xl border-b border-gray-300"
   >
     <slot></slot>
   </span>
