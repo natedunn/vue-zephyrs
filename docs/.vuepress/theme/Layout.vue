fc2391084a057f8748b6267e55cf8da6773c7ed5
Nate Dunn <hello@natedunn.net>
2020-07-24 20:21:55 -0500

    Add starter for custom theme

---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="theme-container">
+    <!-- Sidebar -->
+    <ul v-for="parent in this.$site.locales['/'].sidebar">
+      <li :key="parent.title">
+        Parent: {{ parent.title }}
+        <ul v-for="child in parent.children">
+          <li>
+            <router-link :to="`/${child.path}`">
+              {{ child.title }}
+            </router-link>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <Content />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
